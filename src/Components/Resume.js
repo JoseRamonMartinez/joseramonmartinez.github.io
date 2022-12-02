57705903be612a0f7fec59bacaed34c59e090828
joseramonmartinez <josera.martinez@hotmail.com>
2022-12-02 20:39:51 +0100

    Updated profile data

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -23,7 +23,13 @@ class Resume extends Component {
       var work = this.props.data.work.map(function(work){
         return <div key={work.company}><h3>{work.company}</h3>
             <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-            <p>{work.description}</p>
+            <p>{work.description.split(" ").map((word, index) => {
+              if (word === "Techs:") {
+                return <span key={index}><br/>{word}&nbsp;</span>
+              } else {
+                return <span key={index}>{word} </span>
+              }
+            })}</p>
         </div>
       })
 
